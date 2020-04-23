89a620b47eb6f2ee781dd7778ef8d34a99600e73
KGBcomrade <vlasov.sviatoslav@gmail.com>
2020-04-23 16:56:32 +0300

    Added Entity class

---
+++ Entities/Entity.cpp
@@ -0,0 +1,19 @@
+//
+// Created by kgbcomrade on 4/23/20.
+//
+
+#include "Entity.h"
+
+Entity::Entity(Vector2f position, Texture texture) {
+    this->position = position;
+    this->sprite = new Sprite(texture);
+
+}
+
+Sprite Entity::getSprite() {
+    return Sprite();
+}
+
+void Entity::update(float dt) {
+    sprite->setPosition(position);
+}
---
+++ Entities/Entity.h
@@ -0,0 +1,24 @@
+//
+// Created by kgbcomrade on 4/23/20.
+//
+
+#ifndef VGS_ENTITY_H
+#define VGS_ENTITY_H
+
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+using namespace sf;
+
+class Entity {
+protected:
+    Vector2f position;
+    Sprite *sprite;
+public:
+    Entity(Vector2f position, Texture texture);
+    Sprite getSprite();
+    void update(float dt);
+
+};
+
+
+#endif //VGS_ENTITY_H
